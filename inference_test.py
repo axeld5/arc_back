95f79ce40ce49e0cc84ed539114b6322eb61ec84
axeld5 <axeldarmouni@gmail.com>
2025-09-14 20:55:15 +0200

    update

--- inference_test.py
+++ inference_test.py
@@ -156,23 +156,14 @@ messages = [
             {"role": "user", "content": content},
 ]
 print(messages)
-model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "qwen3_4b_singled_out_sft/final", 
-        max_seq_length = 8192,
-        dtype = torch.bfloat16,
+from unsloth import FastLanguageModel
+base_model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = "unsloth/Qwen2.5-3B-Instruct",
+        max_seq_length = 2048,
         load_in_4bit = True,
     )
-
-model = FastLanguageModel.get_peft_model(
-        model,
-        r=128,
-        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                          "gate_proj", "up_proj", "down_proj",],
-        lora_alpha = 32,  # Best to choose alpha = rank or rank*2
-        lora_dropout = 0, # Supports any, but = 0 is optimized
-        bias = "none",    # Supports any, but = "none" is 
-        use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
-)
+from peft import PeftModel
+model = PeftModel.from_pretrained(base_model, "qwen3_4b_singled_out_sft")
 FastLanguageModel.for_inference(model) # Enable native 2x faster inference
 inputs = tokenizer.apply_chat_template(messages, tokenize = True, add_generation_prompt = True, return_tensors = "pt").to("cuda")
 outputs = model.generate(input_ids = inputs, max_new_tokens = 8192, use_cache = True)
