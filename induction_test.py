c2c0b5cb62f0e17edbfafc3d948eb0fe216de36f
axeld5 <axeldarmouni@gmail.com>
2025-09-18 22:15:12 +0200

    smaller modle

--- induction_test.py
+++ induction_test.py
@@ -123,7 +123,7 @@ with open('test_problems.json', 'w') as f:
 def run_sft(
     dataset_path: str,
     output_dir: str = "qwen3_4b_singled_out_sft",
-    base_model: str = "unsloth/Qwen2.5-Coder-7B-Instruct",
+    base_model: str = "unsloth/Qwen2.5-Coder-3B-Instruct",
     learning_rate: float = 8e-5,
     num_train_epochs: int = 100,
     use_compile: bool = False,
@@ -209,7 +209,7 @@ run_sft("data.json")
 
 from unsloth import FastLanguageModel
 base_model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "unsloth/Qwen2.5-Coder-7B-Instruct",
+        model_name = "unsloth/Qwen2.5-Coder-3B-Instruct",
         max_seq_length = 20000,
         dtype = torch.bfloat16,
         load_in_4bit = True,
