1bbf8a10f898dd07c978accb489719b56a7ed828
axeld5 <axeldarmouni@gmail.com>
2025-10-01 01:31:36 +0200

    update

--- induction_pipeline.py
+++ induction_pipeline.py
@@ -347,7 +347,7 @@ def run_rl(
     grad_accum: int = 4,
     num_generations: int = 4,
 ):
-    max_seq_length = 30000
+    max_seq_length = 20000
     use_bf16 = torch.cuda.is_available() and torch.cuda.get_device_capability(0)[0] >= 8
     compute_dtype = torch.bfloat16 if use_bf16 else torch.float16
     
