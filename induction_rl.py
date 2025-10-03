b38328cf7aeade32c67b2422866b5fb8b0e5928a
axeld5 <axeldarmouni@gmail.com>
2025-10-04 00:17:28 +0200

    added urlib

--- induction_rl.py
+++ induction_rl.py
@@ -8,6 +8,7 @@ from huggingface_hub import login
 from trl import GRPOConfig, GRPOTrainer
 from unsloth import FastLanguageModel, PatchFastRL
 from datasets import Dataset
+from urllib.parse import urlparse
 
 PatchFastRL("GRPO", FastLanguageModel)
 vllm_url = "http://127.0.0.1:8000"  # or read from argparse/env
